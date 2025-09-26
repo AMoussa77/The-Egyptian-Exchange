0a05221a3c73c150966313ca3bea3a8805eb51a8
AMoussa77 <amrmoussa2490@gmail.com>
2025-09-26 13:04:08 +0300

    carry on

--- set-github-token.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Setting GitHub token for Egyptian Exchange Stocks...
-set GITHUB_TOKEN=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
-echo Token set successfully!
-echo.
-echo Starting the application...
-npm start
